025f7d0d715f4bfd5c523d92338b2e12c22c8850
hyang <hyang@hyang-E6410.Burlington.Emptoris.com>
2012-01-21 16:00:02 -0500

    minor changes.

--- src/com/hy/financial/Quote.java
+++ src/com/hy/financial/Quote.java
@@ -1,17 +1,5 @@
 /*
- * ZINNOVA TECHNOLOGIES CONFIDENTIAL
- * __________________________________
- * Copyright (c) 2011 Zinnova Technologies Inc. All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Zinnova Technologies Inc and its suppliers. 
- * The intellectual and technical concepts contained
- * herein are proprietary to Zinnova Technologies Inc
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * The information contained herein may not be transferred, published,
- * disclosed, reproduced, or used by any party without prior and expressive
- * written permission of Zinnova Tehcnologies Inc.
+ * Copyright (c) 2011 Sageville Inc. All Rights Reserved.
  */
 package com.hy.financial;
 
